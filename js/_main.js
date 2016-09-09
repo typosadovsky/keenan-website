1b1cde0cd57287b7fb20ebfc9849fa7fa1a87287
typosadovsky <typosadovsky12@gmail.com>
2016-09-08 22:11:48 -0400

    website

--- js/_main.js
+++ js/_main.js
@@ -1,20 +1,3 @@
- /**
- * This demo was prepared for you by Petr Tichy - Ihatetomatoes.net
- * Want to see more similar demos and tutorials?
- * Help by spreading the word about Ihatetomatoes blog.
- * Facebook - https://www.facebook.com/ihatetomatoesblog
- * Twitter - https://twitter.com/ihatetomatoes
- * Google+ - https://plus.google.com/u/0/109859280204979591787/about
- * Article URL: http://ihatetomatoes.net/simple-parallax-scrolling-tutorial/
- */
-
-/*$("#link_home").hide();
-$("#link_about").hide();
-$("#link_news").hide();
-$("#link_music").hide();
-$("#link_videos").hide();
-$("#link_contact").hide();*/
-
 Responsive();
 
 $(".hamburger-box").hide();
